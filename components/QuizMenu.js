89760cd5c10c06e821aa7090f9b7e260ffe42a84
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-23 13:47:57 -0600

    Refactorizar componentes para mejorar modularidad y reusabilidad

--- components/QuizMenu.js
+++ components/QuizMenu.js
@@ -2,12 +2,16 @@ import QuizMenuItem from "./QuizMenuItem";
 import data from "/data/data.json";
 import "./QuizMenu.css";
 
-const menuItemsHTML = data.quizzes.map((quiz) =>
-  QuizMenuItem(quiz.title, quiz.icon)
-);
-
 function QuizMenu() {
-  return `<ul class='quiz-menu'>${menuItemsHTML.join("")}</ul>`;
+  const ul = document.createElement("ul");
+  ul.className = "quiz-menu";
+
+  data.quizzes.forEach((quiz) => {
+    const menuItem = QuizMenuItem(quiz.title, quiz.icon);
+    ul.appendChild(menuItem);
+  });
+
+  return ul;
 }
 
 export default QuizMenu;
--- components/QuizMenuItem.js
+++ components/QuizMenuItem.js
@@ -1,11 +1,28 @@
 import "./QuizMenuItem.css";
 
 function QuizMenuItem(title, imageSrc) {
-  return `<li class='quiz-menu__item'><button class='quiz-menu__btn' id='${
-    "btn-" + title + "-quiz"
-  }'><span class='quiz-menu__img-container ${
-    title.toLowerCase() + "-icon"
-  }'><img src='${imageSrc}'></span><span>${title}</span></button></li>`;
+  const li = document.createElement("li");
+  li.className = "quiz-menu__item";
+
+  const button = document.createElement("button");
+  button.className = "quiz-menu__btn";
+  button.id = `btn-${title}-quiz`;
+
+  const imgContainer = document.createElement("span");
+  imgContainer.className = `quiz-menu__img-container ${title.toLowerCase()}-icon`;
+
+  const img = document.createElement("img");
+  img.src = imageSrc;
+
+  const span = document.createElement("span");
+  span.textContent = title;
+
+  imgContainer.appendChild(img);
+  button.appendChild(imgContainer);
+  button.appendChild(span);
+  li.appendChild(button);
+
+  return li;
 }
 
 export default QuizMenuItem;
