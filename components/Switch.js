c51659090032a18a6ba39cc719e619378c7b9309
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-27 13:53:53 -0600

    Refactoriza componente switch

--- components/Switch.js
+++ components/Switch.js
@@ -1,19 +1,22 @@
 import "./Switch.css";
 
 function Switch() {
-  const label = document.createElement("label");
-  label.className = "switch";
+  const toggleSwitch = document.createElement("div");
+  toggleSwitch.className = "toggle-switch";
 
-  const input = document.createElement("input");
-  input.type = "checkbox";
+  const toggleInput = document.createElement("input");
+  toggleInput.className = "toggle-input";
+  toggleInput.id = "toggle";
+  toggleInput.type = "checkbox";
 
-  const span = document.createElement("span");
-  span.className = "slider round";
+  const toggleLabel = document.createElement("label");
+  toggleLabel.className = "toggle-label";
+  toggleLabel.setAttribute("for", "toggle");
 
-  label.appendChild(input);
-  label.appendChild(span);
+  toggleSwitch.appendChild(toggleInput);
+  toggleSwitch.appendChild(toggleLabel);
 
-  return label;
+  return toggleSwitch;
 }
 
 export default Switch;
