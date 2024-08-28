204db1659f1c3182d3c0b0fa0440c84e1e681113
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-28 10:28:30 -0600

    Renderiza barra de progreso y agrega estructura base con indicaciones para renderizar los demas componentes.

--- main.js
+++ main.js
@@ -1,8 +1,76 @@
 import QuizMenu from "./components/QuizMenu";
 import ColorModeSwitch from "./components/ColorModeSwitch";
 import QuizHeading from "./components/QuizHeading";
+import ProgressBar from "./components/ProgressBar";
+
+import { quizzes } from "./data/data.json";
 
 import "./style.css";
+
+/* Agrego estas variables 'globales' que nos ayudaran con la logica del quiz, son libres de agregar 
+cualquier otra que consideren necesaria para sus tareas */
+let currentQuiz;
+let currentQuestion = 0;
+let progressBar;
+let score = 0;
+let selectedAnswer;
+
+// En esta funcion generaremos la interfaz inicial del quiz elegido.
+// Cada uno llamara a la funcion que creo para que se renderice su componente
+function startQuiz(quizName) {
+  currentQuiz = quizzes.find((quiz) => quiz.title === quizName);
+  document.body.classList.add("quiz-started");
+
+  /* Alejandra invoca a su funcion renderQuizTopic() y el componente con el icono y titulo del quuiz actual
+  aparece en el header al lado izquierdo del componente switch (modo claro/oscuro), el cual ya se encuentra
+  renderizado */
+
+  renderProgressBar(); /* Marvin invoca a su funcion renderProgressBar() y aparece la barra con el 10% de avance 
+   correspondiente a la pregunta 1, segun se avance a las siguientes preguntas la barra se ira llenando mas */
+
+  /* William invoca a su funcion renderAnswersMenu() y el componente AnswersMenu aparece al lado derecho 
+   del contenedor central, sustituyendo al componente QuizMenu que previamente estaba ahi. El  componente
+   inicialmente muestra las opciones de respuesta correspondientes a la pregunta actual, segun se avance a la 
+   siguiente pregunta, estas opciones deben ir cambiando */
+
+  /* Rafael invoca a su funcion renderQuestion() y notara que no hay un componente para esto
+   ya que tiene que renderizar el mensaje 'Question x of y' y abajito renderizar la pregunta actual (los cuales iran cambiando conforme se avance a las siguientes preguntas), para
+   lo cual debe sustituir el contenido que hay en ese elemento (el titulo 'Welcome to the Frontend Quiz!
+   y el subtitulo 'Pick a subject to get started.') */
+
+  /* Alguien mas invoca a su funcion renderButton()  y el componente Button aparece a final del contenedor
+  derecho (un section con la clase 'answers-section') como ultimo hijo de este, y como hermano del componente que agrego William (el de las opciones de respuesta). El boton inicialmente mostrara el texto 'Submit Answer'. 
+  
+  */
+}
+
+function renderProgressBar() {
+  if (!progressBar) {
+    progressBar = ProgressBar(0);
+    document.querySelector(".questions-section").appendChild(progressBar);
+  }
+  updateProgressBar();
+}
+
+function updateProgressBar() {
+  // Calcula el porcentaje basado en la cantidad de preguntas actuales.
+  const totalQuestions = currentQuiz.questions.length;
+  const percentage = ((currentQuestion + 1) / totalQuestions) * 100;
+
+  // Actualiza el estilo de la barra de progreso.
+  progressBar.style.setProperty(
+    "--current-progress",
+    `${Math.max(100 - percentage, 0)}%`
+  );
+}
+
+// En esta funcion actualizaremos la pregunta actual que se muestra a la izquierda
+// y las opciones de respuesta que aparecen a la derecha. Asi como estoy actualizando
+// la barra de progreso
+function nextQuestion() {
+  currentQuestion++;
+  updateProgressBar(); // Actualiza la barra de progreso con la nueva pregunta.
+}
 
 // Función para crear un elemento con clase
 function createElementWithClass(tag, className) {
@@ -63,7 +131,8 @@ function initApp() {
   const quizMenuButtons = document.querySelectorAll(".quiz-menu__btn");
   quizMenuButtons.forEach((button) =>
     button.addEventListener("click", () => {
-      console.log(`Let's start the ${button.textContent} Quiz`);
+      console.log(`Let's start the ${button.id.split("-")[1]} Quiz`);
+      startQuiz(button.id.split("-")[1]);
     })
   );
 }
