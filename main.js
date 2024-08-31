472ceb878f3001d301f481d4a43fa5beed4f1c5f
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-08-31 08:38:14 -0600

    Implementa libreria de confetti para cuando se alcanza un score de 9 o 10

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { confetti } from "@tsparticles/confetti";
+
 import QuizMenu from "./components/QuizMenu";
 import ColorModeSwitch from "./components/ColorModeSwitch";
 import QuizHeading from "./components/QuizHeading";
@@ -22,6 +24,40 @@ let submitAnswerBtn;
 let questionContainer;
 let questionCount;
 let questionText;
+let confettiInterval;
+
+function launchConfetti() {
+  const duration = 10 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  confettiInterval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(confettiInterval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
+}
 
 function resetQuiz() {
   // Reiniciar las variables globales
@@ -34,6 +70,8 @@ function resetQuiz() {
   questionCount = null;
   questionText = null;
   progressBar = null;
+
+  clearInterval(confettiInterval);
 
   // Limpiar las secciones de preguntas y respuestas
   document.querySelector(".questions-section").innerHTML = "";
@@ -261,6 +299,8 @@ function nextQuestion() {
   currentQuestion++;
 
   if (currentQuestion >= currentQuiz.questions.length) {
+    if (score >= 9) launchConfetti();
+
     const playAgainBtn = Button("Play Again");
     playAgainBtn.addEventListener("click", () => resetQuiz());
     showQuizResult();
